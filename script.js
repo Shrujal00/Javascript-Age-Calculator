71da894628d95ad6bee079f91775649d1141e389
Shrujal00 <shrujal000@gmail.com>
2025-04-01 01:20:03 +0530

    update-it-later

---
+++ script.js
@@ -0,0 +1,16 @@
+let userInput;
+let date;
+let years;
+let months;
+let days;
+
+
+function getDate(){
+    userInput = document.getElementById("date").value;
+    date = new Date(userInput);
+    console.log(date);
+}
+
+function calculate(){
+    
+}
